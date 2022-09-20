d9c45300378da5bf728894de8a4af69d67b17711
Julian Ochsenreiter <ochsenreiter.j04@htlwienwest.at>
2022-09-20 11:49:53 +0200

    client

---
+++ MonopolyClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using MonopolyClient.MonopolySvcRef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MonopolyServiceClient();
+            client.GoToJail();
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
